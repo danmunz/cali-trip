45460354c6f15dcee384ae15f8197e1e917b769f
Dan Munz <daniel.a.munz@gmail.com>
2026-04-07 15:54:44 -0700

    weather

--- src/data/weather.generated.ts
+++ src/data/weather.generated.ts
@@ -36,16 +36,17 @@ export const weatherData: WeatherData = {
       "date": "2026-04-06",
       "displayDate": "Apr 6",
       "location": "Yosemite",
-      "high": 62,
-      "low": 38,
-      "condition": "partly-cloudy",
-      "source": "average"
+      "high": 71,
+      "low": 44,
+      "condition": "sunny",
+      "shortForecast": "Sunny",
+      "source": "forecast"
     },
     {
       "date": "2026-04-07",
       "displayDate": "Apr 7",
       "location": "Yosemite",
-      "high": 72,
+      "high": 70,
       "low": 45,
       "condition": "sunny",
       "shortForecast": "Sunny",
@@ -56,9 +57,9 @@ export const weatherData: WeatherData = {
       "displayDate": "Apr 8",
       "location": "Carmel",
       "high": 71,
-      "low": 52,
+      "low": 53,
       "condition": "sunny",
-      "shortForecast": "Mostly Sunny",
+      "shortForecast": "Sunny",
       "source": "forecast"
     },
     {
@@ -67,16 +68,16 @@ export const weatherData: WeatherData = {
       "location": "Carmel",
       "high": 70,
       "low": 53,
-      "condition": "sunny",
-      "shortForecast": "Partly Sunny",
+      "condition": "thunderstorm",
+      "shortForecast": "Chance Light Rain then Slight Chance Showers And Thunderstorms",
       "source": "forecast"
     },
     {
       "date": "2026-04-10",
       "displayDate": "Apr 10",
       "location": "Carmel",
-      "high": 70,
-      "low": 51,
+      "high": 69,
+      "low": 50,
       "condition": "thunderstorm",
       "shortForecast": "Showers And Thunderstorms Likely",
       "source": "forecast"
@@ -85,12 +86,12 @@ export const weatherData: WeatherData = {
       "date": "2026-04-11",
       "displayDate": "Apr 11",
       "location": "SF",
-      "high": 64,
-      "low": 49,
+      "high": 65,
+      "low": 51,
       "condition": "light-rain",
-      "shortForecast": "Chance Light Rain",
+      "shortForecast": "Slight Chance Light Rain",
       "source": "forecast"
     }
   ],
-  "fetchedAt": "2026-04-07T19:15:03.943Z"
+  "fetchedAt": "2026-04-06T15:30:20.292Z"
 };
